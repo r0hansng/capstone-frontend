87357910cd87d2090c5d32c22028f6dae51a5a8c
Rohan Singh <rohan.singh.en@gmail.com>
2025-11-17 16:46:45 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import Routes from './routes/Routes';
 
-const options1 = [
-  { label: 'React', value: 'react' },
-  { label: 'Vue', value: 'vue' },
-  { label: 'Svelte', value: 'svelte' }
-];
-
 const App = () => {
   return (
     <RouterProvider router={Routes} />
